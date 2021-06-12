8d5b181da235c9d185a52e83945803d01618d6b2
ブーゾ・アモス <buzoamosu@buzoamosunoMacBook-Pro.local>
2021-06-12 12:42:38 +0900

    scaffold Content

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  resources :contents
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
   
 
   Rails.application.routes.draw do
+  resources :contents
     devise_for :users, controllers: {
       sessions: 'users/sessions'
     }
